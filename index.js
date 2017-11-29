21dcd0022b21a0adff2f8c4f32fdd41d4ab9feef
Dmytro Semenov <dsemenov@ebay.com>
2017-11-29 14:06:06 -0800

    added plugin validation

--- index.js
+++ index.js
@@ -1,78 +1,84 @@
 'use strict';
 
-module.exports = {
-    decorate: function (pipe) {
-        pipe.decorate('request', function (original) {
-            return function (request, callback) {
-                if (this.context.$requestSession && this.context.$requestSession.closed !== true) {
-                    throw new Error('Request has already been initiated');
-                }
-                var session = this.context.$requestSession = {};
+var plugin = require('trooba-plugin');
 
-                this.resume();
+module.exports = plugin({
+    decorate: decorate
+}, {
+    troobaVersion: '^3'
+});
 
-                this.send({
-                    type: 'request',
-                    data: request,
-                    session: session
+function decorate(pipe) {
+    pipe.decorate('request', function (original) {
+        return function (request, callback) {
+            if (this.context.$requestSession && this.context.$requestSession.closed !== true) {
+                throw new Error('Request has already been initiated');
+            }
+            var session = this.context.$requestSession = {};
+
+            this.resume();
+
+            this.send({
+                type: 'request',
+                data: request,
+                session: session
+            });
+
+            if (callback) {
+                this.once('error', function (err) {
+                    callback(err);
+                })
+                .once('response', function (response) {
+                    callback(null, response);
                 });
+            }
+            return createStream(this, pipe.Direction.REQUEST, session);
+        };
+    });
 
-                if (callback) {
-                    this.once('error', function (err) {
-                        callback(err);
-                    })
-                    .once('response', function (response) {
-                        callback(null, response);
-                    });
-                }
-                return createStream(this, pipe.Direction.REQUEST, session);
-            };
-        });
+    pipe.decorate('continue', function () {
+        return function (data) {
+            if (data) {
+                this.current.data = data;
+            }
+            this.current.next();
+        };
+    });
 
-        pipe.decorate('continue', function () {
-            return function (data) {
-                if (data) {
-                    this.current.data = data;
-                }
-                this.current.next();
-            };
-        });
+    pipe.decorate('retry', function () {
+        return function (request) {
+            shutdownSession(this);
+            this.request(request);
+        };
+    });
 
-        pipe.decorate('retry', function () {
-            return function (request) {
-                shutdownSession(this);
-                this.request(request);
-            };
-        });
+    pipe.decorate('respond', function (original) {
+        return function (response) {
+            if (this.context.$responseSession && this.context.$responseSession.closed !== true) {
+                throw new Error('Response has already been initiated');
+            }
+            var session = this.context.$responseSession = {};
 
-        pipe.decorate('respond', function (original) {
-            return function (response) {
-                if (this.context.$responseSession && this.context.$responseSession.closed !== true) {
-                    throw new Error('Response has already been initiated');
-                }
-                var session = this.context.$responseSession = {};
+            this.resume();
+            this.send({
+                type: 'response',
+                data: response,
+                session: session,
+                direction: pipe.Direction.RESPONSE
+            });
+            return createStream(this, pipe.Direction.RESPONSE, session);
+        };
+    });
 
-                this.resume();
-                this.send({
-                    type: 'response',
-                    data: response,
-                    session: session,
-                    direction: pipe.Direction.RESPONSE
-                });
-                return createStream(this, pipe.Direction.RESPONSE, session);
-            };
-        });
+    pipe.decorate('throw', function (original) {
+        return function (err) {
+            shutdownSession(this);
+            this.resume();
+            this.send('error', err, pipe.Direction.RESPONSE);
+        };
+    }, true);
 
-        pipe.decorate('throw', function (original) {
-            return function (err) {
-                shutdownSession(this);
-                this.resume();
-                this.send('error', err, pipe.Direction.RESPONSE);
-            };
-        }, true);
-
-    }
-};
+}
 
 function shutdownSession(pipe) {
     if (pipe.context.$responseSession) {
